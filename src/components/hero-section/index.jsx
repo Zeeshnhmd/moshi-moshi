048c6d593b3e94f8e724be1b6471a34cc46cf9b9
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-12-29 20:52:11 +0530

    feat: hero section

--- src/components/hero-section/index.jsx
+++ src/components/hero-section/index.jsx
@@ -1,4 +1,8 @@
 import Navbar from "../navbar";
+import Logo from "../../assets/logo.png";
+import BeerText from "../../assets/beer-text.svg";
+import ScrollDown from "../../assets/scroll-down.svg";
+
 import styles from "./heroSection.module.scss";
 
 const HeroSection = () => {
@@ -7,6 +11,18 @@ const HeroSection = () => {
       <div className={styles["hero-section-wrapper"]}>
         <div className={styles["hero-section"]}>
           <Navbar />
+
+          <div className={styles["hero-content"]}>
+            <img className={styles["logo"]} src={Logo} alt="" />
+            <div className={styles["title"]}>
+              <p className={styles["toit"]}>Toit</p>
+              <img className={styles["beer"]} src={BeerText} alt="" />
+            </div>
+            <div className={styles["scroll-down"]}>
+              <img className={styles["scroll-arrow"]} src={ScrollDown} alt="" />
+              <p className={styles["scroll-down-text"]}>Scroll down</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
