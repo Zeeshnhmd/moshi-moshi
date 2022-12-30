1022a0104c7a20b7ea3b40eeb22191b438a773a5
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-12-30 20:45:00 +0530

    hero and product section responsive

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { MdClose } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 import MenuIcon from "../../assets/menu-icon.svg";
 import FindBeerIcon from "../../assets/find-beer.svg";
 import Logo from "../../assets/logo.png";
@@ -5,6 +9,8 @@ import Logo from "../../assets/logo.png";
 import styles from "./navbar.module.scss";
 
 const Navbar = () => {
+  const [navState, setNavState] = useState(false);
+
   return (
     <nav className={styles["navbar-wrapper"]}>
       <div className={`${styles["navbar"]} container`}>
@@ -17,15 +23,28 @@ const Navbar = () => {
           <span className={styles["brand-name"]}>Menu</span>
         </div>
         <img className={`${styles["logo"]} img-fluid`} src={Logo} alt="" />
-        <ul className={styles["nav-menus"]}>
-          <li className={styles["nav-link"]}>kudix</li>
-          <li className={styles["nav-link"]}>Reservation</li>
-          <li className={styles["nav-link"]}>brewery tour</li>
-          <li className={styles["nav-link"]}>
-            <span>Find My Beer</span>{" "}
-            <img className="img-fluid" src={FindBeerIcon} alt="" />
-          </li>
-        </ul>
+        <div className={styles["toogle-container"]}>
+          {navState ? (
+            <MdClose onClick={() => setNavState(false)} />
+          ) : (
+            <GiHamburgerMenu onClick={() => setNavState(true)} />
+          )}
+        </div>
+        <div
+          className={`${styles["links-container"]} ${
+            navState ? styles["nav-visible"] : ""
+          }`}
+        >
+          <ul className={styles["nav-menus"]}>
+            <li className={styles["nav-link"]}>kudix</li>
+            <li className={styles["nav-link"]}>Reservation</li>
+            <li className={styles["nav-link"]}>brewery tour</li>
+            <li className={styles["nav-link"]}>
+              <span>Find My Beer</span>{" "}
+              <img className="img-fluid" src={FindBeerIcon} alt="" />
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
